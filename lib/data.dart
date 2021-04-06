7b8865a8fe7881b5b78d23b7f01433b0951636bb
Gabriel Caesario <gabrielcaesario@gmail.com>
2021-04-06 19:08:50 +0700

    initial commit

---
+++ lib/data.dart
@@ -0,0 +1,11 @@
+class User {
+  final String givenName;
+  final String familyName;
+  final String imageUrl;
+
+  const User({
+    this.givenName,
+    this.familyName,
+    this.imageUrl,
+  });
+}
